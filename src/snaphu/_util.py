aff0df620bdb75d3d5e8586c74c1c01356e5143c
Geoffrey M Gunter <geoffrey.m.gunter@jpl.nasa.gov>
2023-11-16 09:15:01 +0000

    Add `BlockIterator`

--- src/snaphu/_util.py
+++ src/snaphu/_util.py
@@ -1,13 +1,145 @@
+import itertools
 import os
 import shutil
-from collections.abc import Generator
+from collections.abc import Generator, Iterable, Iterator
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
+import numpy as np
+from numpy.typing import ArrayLike
+
 __all__ = [
+    "BlockIterator",
+    "ceil_divide",
     "scratch_directory",
 ]
+
+
+def ceil_divide(n: ArrayLike, d: ArrayLike) -> np.ndarray:
+    """
+    Return the smallest integer greater than or equal to the quotient of the inputs.
+
+    Computes integer division of dividend `n` by divisor `d`, rounding up instead of
+    truncating.
+
+    Parameters
+    ----------
+    n : array_like
+        The numerator.
+    d : array_like
+        The denominator.
+
+    Returns
+    -------
+    q : numpy.ndarray
+        The quotient, rounded up to the next integer.
+    """
+    n = np.asanyarray(n)
+    d = np.asanyarray(d)
+    return (n + d - np.sign(d)) // d
+
+
+def as_tuple_of_int(ints: int | Iterable[int]) -> tuple[int, ...]:
+    """
+    Convert the input to a tuple of ints.
+
+    Parameters
+    ----------
+    ints : int or iterable of int
+        One or more integers.
+
+    Returns
+    -------
+    out : tuple of int
+        Tuple containing the input(s).
+    """
+    try:
+        return (int(ints),)  # type: ignore[arg-type]
+    except TypeError:
+        return tuple([int(i) for i in ints])  # type: ignore[union-attr]
+
+
+@dataclass(frozen=True)
+class BlockIterator(Iterable[tuple[slice, ...]]):
+    """
+    An iterable over chunks of an N-dimensional array.
+
+    `BlockIterator` represents a partitioning of a multidimensional array into
+    regularly-sized non-overlapping blocks. Each block is represented by an index
+    expression (i.e. a tuple of `slice` objects) that can be used to access the
+    corresponding block of data from the partitioned array. The full set of blocks spans
+    the entire array.
+
+    Iterating over a `BlockIterator` object yields each block in unspecified order.
+    """
+
+    shape: tuple[int, ...]
+    """tuple of int : The shape of the array to be partitioned into blocks."""
+    chunks: tuple[int, ...]
+    """
+    tuple of int : The shape of a typical block. The last block along each axis may be
+    smaller.
+    """
+
+    def __init__(self, shape: int | Iterable[int], chunks: int | Iterable[int]):
+        """
+        Construct a new `BlockIterator` object.
+
+        Parameters
+        ----------
+        shape : int or iterable of int
+            The shape of the array to be partitioned into blocks. Each dimension must be
+            > 0.
+        chunks : int or iterable of int
+            The shape of a typical block. Must be the same length as `shape`. Each chunk
+            dimension must be > 0.
+        """
+        # Normalize `shape` and `chunks` into tuples of ints.
+        shape = as_tuple_of_int(shape)
+        chunks = as_tuple_of_int(chunks)
+
+        if len(chunks) != len(shape):
+            errmsg = (
+                "size mismatch: shape and chunks must have the same number of elements,"
+                f" instead got len(shape) != len(chunks) ({len(shape)} !="
+                f" {len(chunks)})"
+            )
+            raise ValueError(errmsg)
+
+        if not all(n > 0 for n in shape):
+            errmsg = f"shape elements must all be > 0, instead got {shape}"
+            raise ValueError(errmsg)
+        if any(n <= 0 for n in chunks):
+            errmsg = f"chunk elements must all be > 0, instead got {chunks}"
+            raise ValueError(errmsg)
+
+        # XXX Workaround for `frozen=True`.
+        object.__setattr__(self, "shape", shape)
+        object.__setattr__(self, "chunks", chunks)
+
+    def __iter__(self) -> Iterator[tuple[slice, ...]]:
+        """
+        Iterate over blocks in unspecified order.
+
+        Yields
+        ------
+        block : tuple of slice
+            A tuple of slices that can be used to access the corresponding block of data
+            from an array.
+        """
+        # Number of blocks along each array axis.
+        nblocks = ceil_divide(self.shape, self.chunks)
+
+        # Iterate over blocks.
+        for block_ind in itertools.product(*[range(n) for n in nblocks]):
+            # Get the lower & upper index bounds for the current block.
+            start = np.multiply(block_ind, self.chunks)
+            stop = np.minimum(start + self.chunks, self.shape)
+
+            # Yield a tuple of slice objects.
+            yield tuple(itertools.starmap(slice, zip(start, stop)))
 
 
 @contextmanager
--- test/test_util.py
+++ test/test_util.py
@@ -1,9 +1,102 @@
+import itertools
 import tempfile
+from collections.abc import Iterable
 from pathlib import Path
 
+import numpy as np
 import pytest
 
-from snaphu._util import scratch_directory
+from snaphu._util import BlockIterator, ceil_divide, scratch_directory
+
+
+class TestCeilDivide:
+    def test_positive(self):
+        assert ceil_divide(3, 2) == 2
+        assert ceil_divide(4, 2) == 2
+        assert ceil_divide(1, 1_000_000) == 1
+
+    def test_negative(self):
+        assert ceil_divide(-3, 2) == -1
+        assert ceil_divide(3, -2) == -1
+        assert ceil_divide(-3, -2) == 2
+
+        assert ceil_divide(-4, 2) == -2
+        assert ceil_divide(4, -2) == -2
+        assert ceil_divide(-4, -2) == 2
+
+    def test_zero(self):
+        assert ceil_divide(0, 1) == 0
+        assert ceil_divide(-0, 1) == 0
+
+    def test_divide_by_zero(self):
+        with pytest.warns(RuntimeWarning) as record:
+            ceil_divide(1, 0)
+
+        assert len(record) == 1
+        warning = record[0].message.args[0]
+        assert "divide by zero encountered" in warning
+
+    def test_arraylike(self):
+        result = ceil_divide([1, 2, 3, 4, 5], 2)
+        expected = [1, 1, 2, 2, 3]
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestBlockIterator:
+    @pytest.fixture
+    def blocks2d(self) -> BlockIterator:
+        return BlockIterator(shape=(100, 101), chunks=(25, 34))
+
+    def test_is_iterable(self, blocks2d: BlockIterator):
+        assert isinstance(blocks2d, Iterable)
+
+    def test_attrs(self, blocks2d: BlockIterator):
+        assert blocks2d.shape == (100, 101)
+        assert blocks2d.chunks == (25, 34)
+
+    def test_nblocks(self, blocks2d: BlockIterator):
+        nblocks = len(list(blocks2d))
+        assert nblocks == 12
+
+    def test_iter(self, blocks2d: BlockIterator):
+        arr = np.zeros(blocks2d.shape, dtype=np.int32)
+        for block in blocks2d:
+            arr[block] += 1
+        np.testing.assert_array_equal(arr, 1)
+
+    def test_blocks1d(self):
+        blocks = BlockIterator(shape=99, chunks=25)
+        assert blocks.shape == (99,)
+        assert blocks.chunks == (25,)
+
+        starts = [0, 25, 50, 75]
+        stops = [25, 50, 75, 99]
+        for block, start, stop in itertools.zip_longest(blocks, starts, stops):
+            (slice_,) = block
+            assert slice_.start == start
+            assert slice_.stop == stop
+            assert slice_.step is None
+
+    def test_shape_chunks_mismatch(self):
+        pattern = (
+            "^size mismatch: shape and chunks must have the same number of elements,"
+            r" instead got len\(shape\) != len\(chunks\) \(2 != 3\)$"
+        )
+        with pytest.raises(ValueError, match=pattern):
+            BlockIterator(shape=(300, 400), chunks=(3, 4, 5))
+
+    def test_bad_shape(self):
+        pattern = r"^shape elements must all be > 0, instead got \(100, -1\)$"
+        with pytest.raises(ValueError, match=pattern):
+            BlockIterator(shape=(100, -1), chunks=(10, 10))
+
+    def test_bad_chunks(self):
+        pattern = r"^chunk elements must all be > 0, instead got \(10, -1\)$"
+        with pytest.raises(ValueError, match=pattern):
+            BlockIterator(shape=(100, 100), chunks=(10, -1))
+
+    def test_repr(self, blocks2d: BlockIterator):
+        assert repr(blocks2d) == "BlockIterator(shape=(100, 101), chunks=(25, 34))"
 
 
 class TestScratchDirectory:
